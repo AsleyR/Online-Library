5678dd3a7a86246bbcd5dcb32731969da6e0176f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-15 01:13:32 -0600

    Add CreateBook component

---
+++ client/app/(components)/book/CreateBook.tsx
@@ -0,0 +1,212 @@
+"use client"
+
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { books } from "@prisma/client";
+import { useState } from "react"
+
+interface CreateBookProps {
+    newBook: {
+        input: {
+            title: string;
+            author: string;
+            bookReleaseDate: number;
+            tags: string;
+        };
+
+        book: books;
+    }
+}
+
+export default function CreateBook() {
+    const { user, error, isLoading } = useUser()
+
+    const [input, setInput] = useState<CreateBookProps['newBook']['input']>({
+        "title": "",
+        "author": "",
+        "bookReleaseDate": 0,
+        "tags": ""
+    })
+
+    const [book, setBook] = useState<CreateBookProps['newBook']['book']>({
+        "id": "",
+        "title": "",
+        "author": "",
+        "bookReleaseDate": 0,
+        "publishedBy": {
+            "username": user?.nickname || user?.email || "Username",
+            "email": user?.email || ""
+        },
+        "publishedDate": new Date(),
+        "tags": []
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = !Number.isNaN(e.target.valueAsNumber) ? e.target.valueAsNumber : 0
+
+        setInput({
+            ...input,
+            "bookReleaseDate": value
+        })
+    }
+
+    const handleAddTags = () => {
+        setBook((prev) => ({
+            ...book,
+            tags: [...prev.tags, input.tags]
+        }))
+
+        setInput({
+            ...input,
+            "tags": ""
+        })
+    }
+
+    const removeTag = (e: React.MouseEvent<HTMLParagraphElement>) => {
+        const currentTags = [...book.tags]
+        const newTags: string[] = []
+        currentTags.map((tag) => {
+            if (tag !== currentTags[Number(e.currentTarget.id)]) {
+                newTags.push(tag)
+            }
+        })
+
+        setBook({
+            ...book,
+            tags: newTags
+        })
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.currentTarget)
+
+        const json: any = {}
+        formData.forEach((value, prop) => {
+            return json[prop] = value
+        })
+
+        const formBody = JSON.stringify({
+            title: input.title,
+            author: input.author,
+            bookReleaseDate: input.bookReleaseDate,
+            publishedBy: book.publishedBy,
+            tags: book.tags
+        })
+
+        console.log(formBody)
+
+        await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/books/create`, {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: formBody
+        }).catch(err => console.log(err))
+
+        setInput({
+            "author": "",
+            "title": "",
+            "bookReleaseDate": 0,
+            "tags": ""
+        })
+
+        setBook({
+            ...book,
+            "title": "",
+            "author": "",
+            "bookReleaseDate": 0,
+            "tags": [],
+        })
+    }
+
+    return (
+        <form action={'/api/books/create'} method={'POST'}
+            id="create-book-form"
+            className="flex flex-col gap-3"
+            onSubmit={handleSubmit}
+        >
+            <div className="flex gap-2">
+                <label htmlFor="title">Title</label>
+                <input
+                    className="border-2 px-2 border-black focus:outline-none"
+                    type={'text'}
+                    name="title"
+                    id="title"
+                    onChange={handleChange}
+                    value={input.title}
+                />
+            </div>
+            <div className="flex gap-2">
+                <label htmlFor="author">Author</label>
+                <input
+                    className="border-2 px-2 border-black focus:outline-none"
+                    type={'text'}
+                    name="author"
+                    id="author"
+                    onChange={handleChange}
+                    value={input.author}
+                />
+            </div>
+            <div className="flex gap-2">
+                <label htmlFor="bookReleaseDate">Book release date</label>
+                <input
+                    className="border-2 px-2 border-black focus:outline-none"
+                    type={'number'}
+                    name="bookReleaseDate"
+                    id="bookReleaseDate"
+                    onChange={handleNumberChange}
+                    value={input.bookReleaseDate}
+                />
+            </div>
+            <div className="flex items-center gap-2">
+                <label htmlFor="tags">Add tags</label>
+                <input
+                    className="border-2 px-2 border-black focus:outline-none"
+                    type={'text'}
+                    name="tags"
+                    id="tags"
+                    onChange={handleChange}
+                    value={input.tags}
+                />
+                <button
+                    className="bg-gray-200 hover:bg-gray-300 rounded w-8 h-8 flex items-center align-middle justify-center"
+                    type={'button'}
+                    onClick={handleAddTags}
+                >
+                    <FontAwesomeIcon className="" icon={faPlus} />
+                </button>
+            </div>
+            <div className="flex gap-3">
+                {
+                    book.tags.length !== 0 ?
+                        book.tags.map((tag, index) => {
+                            return (
+                                <p key={`${index}-book-tag`}
+                                    id={`${index}`}
+                                    onClick={removeTag}
+                                    className="bg-gray-200 rounded px-3 py-1 cursor-pointer">
+                                    {tag}
+                                </p>
+                            )
+                        }) : <p className="font-bold">No tags added yet</p>
+                }
+            </div>
+            <div className="">
+                <button
+                    type={'submit'}
+                    className="bg-red-500 text-white hover:scale-105 transition-all px-3 py-1 rounded"
+                >
+                    Create New Book
+                </button>
+            </div>
+        </form>
+    )
+}
