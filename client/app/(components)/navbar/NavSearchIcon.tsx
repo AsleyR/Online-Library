98ea0880b1033d03fbfc1cdd46aa43910355694f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-15 01:13:13 -0600

    Add NavSearchIcon component

---
+++ client/app/(components)/navbar/NavSearchIcon.tsx
@@ -0,0 +1,19 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+interface NavSearchIconProps {
+    className?: string;
+    link?: string
+}
+
+export default function NavSearchIcon({ className, link }: NavSearchIconProps) {
+    return (
+        <Link
+            className={`${className || ""} w-10 h-10`}
+            href={link || "/books"}
+        >
+            <FontAwesomeIcon className="text-gray-700 w-min" icon={faSearch} />
+        </Link>
+    )
+}
