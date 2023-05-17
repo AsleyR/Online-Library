749e4a1c4ce29299b16c3ab83e45fb61c8595625
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-17 04:34:57 -0600

    Update Comment component

--- client/app/(components)/comments/Comment.tsx
+++ client/app/(components)/comments/Comment.tsx
@@ -1,17 +1,59 @@
+"use client"
+
 import { comments } from "@prisma/client";
+import CommentOptions from "./comment-options/CommentOptions";
+import DefaultUserIcon from "../user-icons/DefaultUserIcon";
+import { CommentOptionsType } from "@/app/(libs)/context/CommentOptions";
+import { createContext, useEffect, useState } from "react";
+import EditCommentForm from "./EditCommentForm";
+import UserIcon from "../user-icons/UserIcon";
+import deleteComment from "@/app/(libs)/deleteComment";
+import { useRouter } from "next/navigation";
+import deleteBookById from "@/app/(actions)/books/delete/deleteBookById";
+import deleteCommentById from "@/app/(actions)/comments/delete/deleteCommentById";
+
+export const CommentOptionsContext = createContext<CommentOptionsType | null>(null)
 
 export default function Comment({ comment }: { comment: comments }) {
-    const day = comment.publishedDate.getDate()
-    const month = comment.publishedDate.getMonth()
-    const year = comment.publishedDate.getFullYear()
+    const router = useRouter()
+    const [commentOptions, setCommentOptions] = useState<CommentOptionsType['commentOptions']>({
+        "render": false,
+        "edit": false,
+        "delete": false
+    })
+
+    // const deleteComment = async () => {
+    //     if (commentOptions.delete) {
+    //         await deleteCommentById(comment.id)
+    //     }
+    // }
+
+    // deleteComment()
+
+    // useEffect(() => {
+    //     deleteComment(comment.id)
+    //     setCommentOptions({ render: false, edit: false, delete: false })
+    // }, [commentOptions.delete === true])
 
     return (
-        <div className="flex flex-col gap-2 bg-gray-200 rounded-lg p-3">
-            <div className="">
-                <h1 className="font-medium text-lg">{comment.author.username}</h1>
-                <span>{`${day}-${month}-${year}`}</span>
+        <CommentOptionsContext.Provider value={{ commentOptions, setCommentOptions }}>
+            <div className="grid grid-cols-[min-content_auto_min-content] items-start gap-3 bg-gray-200 rounded-lg p-3">
+                <UserIcon auth={true} picture={comment.author.profilePicture} className="w-10 h-10" />
+                <div className="">
+                    <div className="flex flex-col md:flex-row gap-1 md:items-center">
+                        <h1 className="font-medium text-lg">{comment.author.username}</h1>
+                        <p className="hidden md:block">•</p>
+                        <span className="-mt-[0.6rem] md:mt-0 text-sm">{`${comment.publishedDate.toDateString()}`}</span>
+                    </div>
+                    {
+                        commentOptions.edit ? <EditCommentForm comment={comment} /> : <p className="">{comment.comment}</p>
+                    }
+                </div>
+                <CommentOptions
+                    commentId={comment.id}
+                    author={comment.author}
+                />
             </div>
-            <p>{comment.comment}</p>
-        </div>
+        </CommentOptionsContext.Provider>
     )
 }
