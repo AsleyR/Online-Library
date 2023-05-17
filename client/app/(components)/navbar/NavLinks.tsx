b7a205ecfa8ca71be74eb60f76bc020fc70c74db
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-17 04:35:39 -0600

    Update NavLinks

--- client/app/(components)/navbar/NavLinks.tsx
+++ client/app/(components)/navbar/NavLinks.tsx
@@ -6,53 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image"
 import Link from "next/link"
 import NavSearchIcon from "./NavSearchIcon"
-
-
-interface UserIconProps {
-    auth: boolean;
-    picture: string | null | undefined;
-    className?: string
-}
-
-export const DefaultUserIcon = () => {
-    return (
-        <div className="bg-[#cfcfcf] p-2 rounded-full w-10 h-10 flex justify-center align-middle items-center">
-            <FontAwesomeIcon className="text-white w-min" icon={faUserAlt} />
-        </div>
-    )
-}
-
-export const UserIcon = ({ auth, picture, className }: UserIconProps) => {
-
-    if (!auth) {
-        return (
-            <Link className={`${className || ""}`} href={'/api/auth/login'}>
-                <DefaultUserIcon />
-            </Link>
-        )
-    }
-
-
-    return (
-        <Link
-            className={`${className || ""}`}
-            href={'/user'}
-        >
-            {
-                picture ?
-                    <Image
-                        className="rounded-full w-10 h-10"
-                        width={100}
-                        height={100}
-                        src={picture}
-                        alt="user icon"
-                    />
-                    : <DefaultUserIcon />
-            }
-
-        </Link>
-    )
-}
+import UserIcon from "../user-icons/UserIcon"
 
 const AddBookButton = () => {
     return (
@@ -72,11 +26,11 @@ const NavLinks = () => {
     return (
         <div className="flex justify-end items-center">
             <ul className="flex gap-3 md:gap-5 justify-self-end items-center align-middle">
-                <NavSearchIcon className="hidden md:flex justify-center items-center text-xl" />
+                <NavSearchIcon className="flex justify-center items-center text-xl" />
                 {
                     user ? <AddBookButton /> : null
                 }
-                <UserIcon className="" auth={user ? true : false} picture={user?.picture} />
+                <UserIcon className="w-10 h-10" auth={user ? true : false} picture={user?.picture} />
             </ul>
         </div>
     )
