d962959f91a9732ab434e76b6616e07af51e5b35
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-17 04:35:04 -0600

    Update Comments component

--- client/app/(components)/comments/Comments.tsx
+++ client/app/(components)/comments/Comments.tsx
@@ -1,24 +1,39 @@
 import { comments } from "@prisma/client";
 import Comment from "./Comment";
+import AddComments from "./AddComments";
 
-export default function Comments({ comments }: { comments: comments[] | null }) {
+interface CommentsProps {
+    comments: comments[] | null;
+    bookId: string;
+}
+
+export default function Comments({ comments, bookId }: CommentsProps) {
     if (!comments) {
         return (
-            <div className="">
-                <h1>No comments</h1>
+            <div className="flex flex-col gap-3 max-w-none md:max-w-xl transition-all">
+                <h1 className='font-bold text-xl'>
+                    0 Comments
+                </h1>
+                <AddComments bookId={bookId} />
             </div>
         )
     }
 
     return (
-        <div className="flex flex-col gap-3">
-            {
-                comments.map((comment, index) => {
-                    return (
-                        <Comment key={`${index}-comment`} comment={comment} />
-                    )
-                })
-            }
+        <div className="flex flex-col gap-3 max-w-none md:max-w-xl transition-all">
+            <h1 className='font-bold text-xl'>
+                {comments?.length} {comments?.length === 1 && comments?.length ? "Comment" : "Comments"}
+            </h1>
+            <AddComments bookId={bookId} />
+            <div className="flex flex-col gap-3">
+                {
+                    comments.map((comment, index) => {
+                        return (
+                            <Comment key={`${index}-comment`} comment={comment} />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
