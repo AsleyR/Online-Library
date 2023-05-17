104155fcebe56ef694deeed855439896fe16d57b
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-17 04:35:32 -0600

    Update Navbar

--- client/app/(components)/navbar/Navbar.tsx
+++ client/app/(components)/navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import NavSearchIcon from "./NavSearchIcon"
 
 const Navbar = () => {
     return (
-        <div className="sticky top-0 z-30 grid grid-cols-3 md:grid-cols-2 h-[4rem] px-mobilex md:px-normalx bg-blue-300 items-center">
-            <NavSearchIcon className="flex md:hidden justify-center items-center text-xl" />
+        <div className="sticky top-0 z-30 grid grid-cols-[auto_min-content_min-content] md:grid-cols-2 h-[4rem] px-mobilex md:px-normalx bg-blue-300 items-center">
             <div className="">
                 <Link className="font-bold text-xl" href={'/'}>Online Library</Link>
             </div>
+            {/* <NavSearchIcon className="flex md:hidden justify-center items-center text-xl" /> */}
             <NavLinks />
         </div>
     )
