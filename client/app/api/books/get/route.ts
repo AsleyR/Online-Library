e579a7a3949d8247fd5ca8ab16de95980174c356
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:11:35 -0600

    Delete bloat

--- client/app/api/books/get/route.ts
+++ client/app/api/books/get/route.ts
@@ -1,15 +1,10 @@
 import { prisma } from "@/app/(libs)/client"
 
-/**
- * Takes the search params 'userEmail' to get documents that match.
- */
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
-    // const res = await request.json()
     let reqObject: any = null
 
     if (searchParams.has('userEmail')) {
-        console.log(searchParams.get('userEmail'))
         reqObject = {
             "where": {
                 "publishedBy": {
