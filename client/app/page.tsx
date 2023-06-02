1ffb5cb50853414d4aa57f13f519e8a1ac8d4779
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:12:37 -0600

    Add hero section to homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,30 +1,49 @@
-"use client"
-
-import { useUser } from '@auth0/nextjs-auth0/client'
+import Image from 'next/image'
 import Link from 'next/link'
+import Container from './(components)/Container'
+import ButtonLink from './(components)/ButtonLink'
+
+const HeroImage = () => {
+  return (
+    <div className="hidden lg:grid justify-end">
+      <Image
+        className=''
+        priority
+        width={1000}
+        height={1000}
+        src={'/images/read-book.jpg'}
+        alt='Person reading book'
+      />
+    </div>
+  )
+}
+
+const Hero = () => {
+  return (
+    <div className='bg-black/10'>
+      <Container className='grid grid-cols-1 lg:grid-cols-2 gap-5 items-center align-middle'>
+        <div className="flex flex-col gap-[2rem]">
+          <h1 className='font-bold text-3xl md:text-5xl'>Knowledge is power.</h1>
+          <h2 className='text-xl md:text-3xl'>Read and share your favorite books with users!</h2>
+          <div className="flex">
+            <ButtonLink
+              className=''
+              text='Open Library'
+              link='/books'
+            />
+          </div>
+        </div>
+        <HeroImage />
+      </Container>
+    </div>
+  )
+}
 
 export default function Home() {
-  const { user, error, isLoading } = useUser()
-
-  if (isLoading) return <div>Loading...</div>
-
-  if (error) return <div>{error.message}</div>
 
   return (
-    <main className="grid gap-3">
-      <h1 className='font-bold text-3xl'>Welcome to your online library!</h1>
-      <div className="">
-        <div className="">
-          <Link
-            className=''
-            href={'/books'}
-          >
-            <button className='bg-blue-400 text-white hover:scale-105 transition-all px-3 py-2 rounded'>
-              See books
-            </button>
-          </Link>
-        </div>
-      </div>
+    <main className="grid">
+      <Hero />
     </main>
   )
 }
