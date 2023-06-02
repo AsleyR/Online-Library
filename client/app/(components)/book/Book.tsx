2064ef2aba93e7728e4a2201d62314d90c97713b
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:17:36 -0600

    Add BookUserAuthor, Add BookCover, Update layout

--- client/app/(components)/book/Book.tsx
+++ client/app/(components)/book/Book.tsx
@@ -1,33 +1,36 @@
 import { books } from "@prisma/client"
 import BookOptionsIcon from "./BookOptionsIcon"
+import BookUserAuthor from "./BookUserAuthor"
+import BookCover from "./BookCover"
+import { UserProfileInfo } from "@/app/(libs)/types"
+import Tag from "../tag"
 
-export default function Book({ book }: { book: books }) {
+export default function Book({ book, bookUser }: { book: books, bookUser: UserProfileInfo }) {
+
     return (
         <>
             <div className="grid grid-cols-[auto_min-content] w-full">
-                <div className="grid">
-                    <h1 className='font-bold text-3xl'>{book.title}</h1>
-                    <h3 className='text-xl italic'>{book.author}</h3>
-                    <p className=''>Book release date: <span className="">{book.bookReleaseDate}</span></p>
-                    <div className="flex flex-col md:flex-row md:gap-1 md:items-center">
-                        <h1>Uploaded by:</h1>
-                        <div className="flex flex-col gap-1 md:flex-row">
-                            <p className="font-medium">{book.publishedBy.username}</p>
-                            <p className="hidden md:block">•</p>
-                            <span className="">{book.publishedDate.toDateString()}</span>
-                        </div>
+                <div className="grid grid-rows-auto md:grid-cols-[min-content_auto] gap-5">
+                    <div className="">
+                        <BookCover cover={book.cover} />
                     </div>
-                    <div className="grid gap-1 mt-3">
-                        <h2 className="font-medium">Genre</h2>
-                        <div className="flex flex-wrap gap-2">
-                            {
-                                book.tags.map((tag, index) => {
-                                    return (
-                                        <p key={`${index}-tag`} className="bg-gray-200 py-1 px-2 rounded-lg">{tag}</p>
-                                    )
-                                })
-                            }
+                    <div className="">
+                        <h1 className='font-bold text-3xl'>{book.title}</h1>
+                        <h3 className='text-lg italic'>{book.author}</h3>
+                        <p className=''>{book.bookReleaseDate}</p>
+                        <div className="grid gap-1 mb-5">
+                            <h2 className="font-medium">Genre</h2>
+                            <div className="flex flex-wrap gap-2 max-w-sm">
+                                {
+                                    book.tags.map((tag, index) => {
+                                        return (
+                                            <Tag key={`${index}-{tag}-tag`} text={tag} />
+                                        )
+                                    })
+                                }
+                            </div>
                         </div>
+                        <BookUserAuthor book={book} user={bookUser} />
                     </div>
                 </div>
                 <div className="flex justify-end">
