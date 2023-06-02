cfe8087a17e3e277e6e38dc9e8200ae7e025cc59
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:07:58 -0600

    Update tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
       padding: {
         "mobilex": "1rem",
         "normalx": "5rem"
+      },
+      colors: {
+        "pr-red": "#EF4444",
+        "pr-green": "#16A34A",
+        "pr-blue": "#3B82F6",
+        "pr-gray": "#E5E7EB",
+        "sc-black": "#333333"
       }
     },
   },
