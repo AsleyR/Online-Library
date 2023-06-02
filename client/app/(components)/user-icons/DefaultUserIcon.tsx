2c057e7d7b6c358294f7ed32320e302195492786
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:50:42 -0600

    Update DefaultUserIcon component

--- client/app/(components)/user-icons/DefaultUserIcon.tsx
+++ client/app/(components)/user-icons/DefaultUserIcon.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function DefaultUserIcon() {
     return (
-        <div className="bg-[#cfcfcf] p-2 rounded-full w-10 h-10 flex justify-center align-middle items-center">
+        <div className="bg-blue-300 p-2 w-10 h-10 rounded-full flex justify-center align-middle items-center">
             <FontAwesomeIcon className="text-white w-min" icon={faUserAlt} />
         </div>
     )
