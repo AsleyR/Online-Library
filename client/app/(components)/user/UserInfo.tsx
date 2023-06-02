d89d90642ae0b9eeecc11f2a7513b20ef83a213a
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:50:29 -0600

    Update UserInfo component

--- client/app/(components)/user/UserInfo.tsx
+++ client/app/(components)/user/UserInfo.tsx
@@ -1,54 +1,46 @@
-"use client"
-
-import { useUser } from "@auth0/nextjs-auth0/client"
+import { UserProfile } from "@auth0/nextjs-auth0/client"
 import { books } from "@prisma/client"
-import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import ButtonLink from "../ButtonLink"
+import { UserProfileInfo } from "@/app/(libs)/types"
 import UserBooks from "../book/UserBooks"
+import filterBooksByEmail from "@/app/(libs)/filterBooksByEmail"
 
-const LogoutButton = () => {
-    return (
-        <button className='bg-red-500 hover:scale-105 transition-all text-white px-5 py-2 rounded'>
-            <Link href={'/api/auth/logout'}>Logout</Link>
-        </button>
-    )
+interface UserInfoProps {
+    user?: UserProfileInfo | UserProfile;
+    books: books[];
+    auth: boolean
 }
 
-export default function UserInfo({ books }: { books: books[] }) {
-    const router = useRouter()
-    const { user, error, isLoading } = useUser()
+export default function UserInfo({ user, books, auth }: UserInfoProps) {
 
-    let userBooks: any = []
-    books.map((book) => {
-        if (book.publishedBy.email === user?.email) {
-            userBooks.push(book)
-        }
-    })
 
-    console.log(userBooks)
-
-    useEffect(() => {
-        if (!user) {
-            router.push('/api/auth/login')
-        }
-    }, [user])
-
-    if (!user) {
-        return <></>
-    }
+    const userBooks = filterBooksByEmail(books, user?.email || "")
 
     return (
         <>
-            <div className="">
-                <h1 className='font-bold text-2xl'>Account Info</h1>
-                <p>Username: <span className=''>{user?.nickname}</span></p>
-                <p>Email: <span className='underline'>{user?.email}</span></p>
+            <div className="grid gap-5 border border-gray-300 rounded p-5 drop-shadow-sm">
+                <h1 className='font-bold text-2xl'>Account info</h1>
+                <div className="">
+                    <div className="flex flex-col border-t border-gray-300 py-3">
+                        <h3 className="font-medium text-lg">Username</h3>
+                        <p>{user?.nickname || "User"}</p>
+                    </div>
+                    <div className={`flex flex-col ${auth ? "border-y" : "border-t"} border-gray-300 py-3`}>
+                        <h3 className="font-medium text-lg">Email</h3>
+                        <p>{user?.email || "user email"}</p>
+                    </div>
+                </div>
+                {
+                    auth ?
+                        <div className="flex">
+                            <ButtonLink
+                                className="bg-red-500 hover:bg-red-600 transition-all text-white px-5 py-2 rounded"
+                                text="Logout"
+                                link="/api/auth/logout" />
+                        </div> : null
+                }
             </div>
-            <div className="flex justify-center">
-                <LogoutButton />
-            </div>
-            <UserBooks books={userBooks} />
+            <UserBooks books={userBooks} user={user} auth={auth} />
         </>
     )
 }
