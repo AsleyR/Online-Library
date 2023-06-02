57c1bc721dcf0484bd4e0f588a7b4a9626386f04
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:20:22 -0600

    Add UserInfoIcon component

---
+++ client/app/(components)/user-icons/UserInfoIcon.tsx
@@ -0,0 +1,24 @@
+import getFullDateWithoutWeekDate from "@/app/(libs)/getFullDateWithoutWeekDate"
+import { UserProfileInfo } from "@/app/(libs)/types"
+import UserIcon from "./UserIcon"
+import capitalizeFirstLetter from "@/app/(libs)/capitalizeFirstLetter"
+
+interface UserInfoIconProps {
+    user: UserProfileInfo
+}
+
+export default function UserInfoIcon({ user }: UserInfoIconProps) {
+    const parsedDate = getFullDateWithoutWeekDate(new Date(user.created_at || ""))
+
+    return (
+        <div className="flex gap-2 items-center">
+            <UserIcon auth={true} picture={user.picture} link="/"
+                className="w-[3.5rem] h-[3.5rem] cursor-default"
+            />
+            <div className="flex flex-col">
+                <h1 className="font-bold text-3xl">{capitalizeFirstLetter(user.nickname || user.name || "User")}</h1>
+                <p className="">Joined {parsedDate}</p>
+            </div>
+        </div>
+    )
+}
