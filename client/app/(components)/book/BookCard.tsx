3a713dd3dfd6131fba7d54c1582d8c9728e628d1
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:17:54 -0600

    Add book cover to BookCard

--- client/app/(components)/book/BookCard.tsx
+++ client/app/(components)/book/BookCard.tsx
@@ -1,11 +1,25 @@
 import { books } from "@prisma/client"
+import Image from "next/image"
 
 export default function BookCard({ book }: { book: books }) {
+    const defaultBookCover = '/images/default-book-cover.png'
+
     return (
-        <div className="bg-gray-100 p-5 rounded-lg drop-shadow-md w-full hover:scale-105 transition-all">
-            <h1 className='font-bold text-lg'>{book.title}</h1>
-            <h3 className='font-text-lg'>{book.author}</h3>
-            <p className='italic font-light'>{book.bookReleaseDate}</p>
+        <div className="bg-gray-100 border border-gray-300 rounded-lg drop-shadow-md w-full hover:scale-105 transition-all">
+            <div className="overflow-clip h-[7rem] rounded-t-lg">
+                <Image
+                    className=""
+                    width={400}
+                    height={400}
+                    src={book.cover || defaultBookCover}
+                    alt="Book cover"
+                />
+            </div>
+            <div className="p-5">
+                <h1 className='font-bold text-lg'>{book.title}</h1>
+                <h3 className='font-text-lg'>{book.author}</h3>
+                <p className='italic font-light'>{book.bookReleaseDate}</p>
+            </div>
         </div>
     )
 }
