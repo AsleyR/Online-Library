0d544aab754760ebe4c53abb2bfe94b99426de1d
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:23:22 -0600

    Add Container to books' page component

--- client/app/books/layout.tsx
+++ client/app/books/layout.tsx
@@ -1,7 +1,11 @@
+import Container from "../(components)/Container";
+
 export default function BooksLayout({ children }: { children: React.ReactNode }) {
     return (
         <section className="" >
-            {children}
+            <Container>
+                {children}
+            </Container>
         </section>
     )
 }
