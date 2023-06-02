f35d0a32e1e8f8f880032bf9a672dd19f8b8b4cb
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 13:32:48 -0600

    Add SearchBar component

--- client/app/(components)/search/SearchBar.tsx
+++ client/app/(components)/search/SearchBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { faX } from "@fortawesome/free-solid-svg-icons"
+import { faSliders, faX } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
 
@@ -11,7 +11,7 @@ interface SearchBarProps {
     }
     props: {
         value?: string;
-        filter?: string
+        filter?: string;
     }
 }
 
@@ -20,6 +20,8 @@ const SearchBar = ({ value, filter }: SearchBarProps['props']) => {
         search: value || "",
         filter: filter || "title"
     })
+
+    const [settings, setSettings] = useState<boolean>(false)
 
     const isRadioSelected = (value: string): boolean => {
         return input.filter === value
@@ -41,8 +43,8 @@ const SearchBar = ({ value, filter }: SearchBarProps['props']) => {
 
     const deleteInput = () => {
         setInput({
-            search: "",
-            filter: ""
+            ...input,
+            search: ""
         })
     }
 
@@ -57,7 +59,7 @@ const SearchBar = ({ value, filter }: SearchBarProps['props']) => {
                     id="search"
                     onChange={handleChange}
                     value={input.search}
-                    placeholder="Search..."
+                    placeholder="Search"
                 />
                 {
                     input.search.length !== 0 ?
@@ -71,52 +73,70 @@ const SearchBar = ({ value, filter }: SearchBarProps['props']) => {
                         : null
                 }
             </div>
-            <fieldset className="flex gap-3">
-                <div className="">
-                    <input
-                        type={'radio'}
-                        id="title"
-                        name="filter"
-                        value={'title'}
-                        checked={isRadioSelected('title')}
-                        onChange={handleRadioClick}
-                    />
-                    <label htmlFor="title">Title</label>
-                </div>
-                <div className="">
-                    <input
-                        type={'radio'}
-                        id="author"
-                        name="filter"
-                        value={'author'}
-                        checked={isRadioSelected('author')}
-                        onChange={handleRadioClick}
-                    />
-                    <label htmlFor="author">Author</label>
-                </div>
-                <div className="">
-                    <input
-                        type={'radio'}
-                        id="year"
-                        name="filter"
-                        value={'year'}
-                        checked={isRadioSelected('year')}
-                        onChange={handleRadioClick}
-                    />
-                    <label htmlFor="year">Year</label>
-                </div>
-                <div className="">
-                    <input
-                        type={'radio'}
-                        id="tags"
-                        name="filter"
-                        value={'tags'}
-                        checked={isRadioSelected('tags')}
-                        onChange={handleRadioClick}
-                    />
-                    <label htmlFor="tags">Tags</label>
-                </div>
-            </fieldset>
+            <div className="flex flex-col gap-3 border-b border-gray-300 py-3">
+                <button
+                    type={'button'}
+                    className="flex w-min"
+                    onClick={() => setSettings(!settings)}
+                >
+                    <div className="flex gap-2 hover:bg-gray-200 rounded-2xl cursor-pointer px-3 py-1">
+                        <h1 className="font-medium">Filters</h1>
+                        <div className="">
+                            <FontAwesomeIcon icon={faSliders} />
+                        </div>
+                    </div>
+                </button>
+                {
+                    settings ?
+                        <fieldset className="flex gap-3">
+                            <div className="flex gap-1">
+                                <input
+                                    type={'radio'}
+                                    id="title"
+                                    name="filter"
+                                    value={'title'}
+                                    checked={isRadioSelected('title')}
+                                    onChange={handleRadioClick}
+                                />
+                                <label htmlFor="title">Title</label>
+                            </div>
+                            <div className="flex gap-1">
+                                <input
+                                    type={'radio'}
+                                    id="author"
+                                    name="filter"
+                                    value={'author'}
+                                    checked={isRadioSelected('author')}
+                                    onChange={handleRadioClick}
+                                />
+                                <label htmlFor="author">Author</label>
+                            </div>
+                            <div className="flex gap-1">
+                                <input
+                                    type={'radio'}
+                                    id="year"
+                                    name="filter"
+                                    value={'year'}
+                                    checked={isRadioSelected('year')}
+                                    onChange={handleRadioClick}
+                                />
+                                <label htmlFor="year">Year</label>
+                            </div>
+                            <div className="flex gap-1">
+                                <input
+                                    type={'radio'}
+                                    id="genre"
+                                    name="filter"
+                                    value={'tags'}
+                                    checked={isRadioSelected('tags')}
+                                    onChange={handleRadioClick}
+                                />
+                                <label htmlFor="genre">Genre</label>
+                            </div>
+                        </fieldset>
+                        : null
+                }
+            </div>
         </form>
     )
 }
