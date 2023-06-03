a4db317222d8e0945d39508fefc7831e99cf6ea4
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-02 18:47:13 -0600

    Fix escaping chars problem

--- client/app/(components)/user/UserNotFound.tsx
+++ client/app/(components)/user/UserNotFound.tsx
@@ -4,7 +4,7 @@ export default function UserNotFound() {
     return (
         <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <div className="flex flex-col justify-center text-center gap-5">
-                <h1 className="font-bold text-3xl">This user doesn't exist</h1>
+                <h1 className="font-bold text-3xl">{`This user doesn't exist`}</h1>
                 <div className="flex justify-center">
                     <ButtonLink
                         className=""
