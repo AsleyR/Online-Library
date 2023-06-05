a168b965102190af19a0031716e4c2430d9a24e2
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-05 18:16:54 -0400

    Implement MobileComments component

--- client/app/(components)/comments/Comments.tsx
+++ client/app/(components)/comments/Comments.tsx
@@ -1,8 +1,9 @@
 import { comments } from "@prisma/client";
 import Comment from "./Comment";
 import AddComments from "./AddComments";
+import MobileComments from "./MobileComments";
 
-interface CommentsProps {
+export interface CommentsProps {
     comments: comments[] | null;
     bookId: string;
 }
@@ -10,30 +11,39 @@ interface CommentsProps {
 export default function Comments({ comments, bookId }: CommentsProps) {
     if (!comments) {
         return (
-            <div className="flex flex-col gap-3 max-w-none md:max-w-xl transition-all">
-                <h1 className='font-bold text-lg'>
-                    0 Comments
-                </h1>
-                <AddComments bookId={bookId} />
-            </div>
+            <>
+                <MobileComments comments={comments} bookId={bookId} />
+                <div className="hidden md:flex flex-col gap-3 max-w-none md:max-w-xl transition-all">
+                    <h1 className='font-bold text-lg'>
+                        0 Comments
+                    </h1>
+                    <AddComments bookId={bookId} />
+                </div>
+            </>
         )
     }
 
     return (
-        <div className="grid grid-flow-row gap-3 max-w-full md:max-w-xl transition-all">
-            <h1 className='font-bold text-lg'>
-                {comments?.length} {comments?.length === 1 && comments?.length ? "Comment" : "Comments"}
-            </h1>
-            <AddComments bookId={bookId} />
-            <div className="flex flex-col gap-3">
-                {
-                    comments.map((comment, index) => {
-                        return (
-                            <Comment key={`${index}-comment`} comment={comment} />
-                        )
-                    })
-                }
+        <>
+            <MobileComments comments={comments} bookId={bookId} />
+            <div className="hidden md:grid grid-flow-row gap-3 transition-all">
+                <h1 className='font-bold text-lg'>
+                    {comments?.length} {comments?.length === 1 && comments?.length ? "Comment" : "Comments"}
+                </h1>
+                <AddComments className="bg-gray-200" bookId={bookId} />
+                <div className="flex flex-col gap-3">
+                    {
+                        comments.map((comment, index) => {
+                            return (
+                                <Comment key={`${index}-comment`}
+                                    className="bg-gray-200"
+                                    comment={comment}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </div>
-        </div>
+        </>
     )
 }
