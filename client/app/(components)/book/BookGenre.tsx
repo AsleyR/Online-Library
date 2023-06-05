b3b3a2ea20cdda46859dc78514fa5832860a0e7b
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-05 18:15:40 -0400

    update css media breakpoint

--- client/app/(components)/book/BookGenre.tsx
+++ client/app/(components)/book/BookGenre.tsx
@@ -5,7 +5,7 @@ export default function BookGenre({ genres }: BookGenreProps) {
     return (
         <div className="grid gap-1 mb-5">
             <h2 className="font-medium">Genre</h2>
-            <div className="flex flex-wrap gap-2 max-w-sm">
+            <div className="flex flex-wrap gap-2 lg:max-w-sm">
                 {
                     genres.map((genre, index) => {
                         return (
