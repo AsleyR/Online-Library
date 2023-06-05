510efb3eb87ebebf26f293e3f650ebc738b6e017
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-05 18:51:57 -0400

    Made UserIcon be able to not link to user's page

--- client/app/(components)/user-icons/UserIcon.tsx
+++ client/app/(components)/user-icons/UserIcon.tsx
@@ -7,30 +7,56 @@ export default function UserIcon({ auth, picture, link, className }: UserIconPro
 
     if (!auth) {
         return (
-            <Link className={`${className || ""} w-10 h-10`} href={'/api/auth/login'}>
-                <DefaultUserIcon />
-            </Link>
+            <>
+                {
+                    link !== null ?
+                        <Link className={`${className || ""} w-10 h-10`} href={'/api/auth/login'}>
+                            <DefaultUserIcon />
+                        </Link> :
+                        <div className={`${className || ""} w-10 h-10`}>
+                            <DefaultUserIcon />
+                        </div>
+                }
+            </>
         )
     }
 
 
     return (
-        <Link
-            className={`${className || ""} w-10 h-10`}
-            href={link ? link : '/user'}
-        >
+        <>
             {
-                picture !== null && picture !== undefined ?
-                    <Image
-                        className="rounded-full"
-                        width={500}
-                        height={500}
-                        src={picture}
-                        alt="user icon"
-                    />
-                    : <DefaultUserIcon />
-            }
+                link !== null ?
+                    <Link
+                        className={`${className || ""} w-10 h-10`}
+                        href={link ? link : '/user'}
+                    >
+                        {
+                            picture !== null && picture !== undefined ?
+                                <Image
+                                    className="rounded-full"
+                                    width={500}
+                                    height={500}
+                                    src={picture}
+                                    alt="user icon"
+                                />
+                                : <DefaultUserIcon />
+                        }
 
-        </Link>
+                    </Link> :
+                    <div className={`${className || ""} w-10 h-10`}>
+                        {
+                            picture ?
+                                <Image
+                                    className="rounded-full"
+                                    width={500}
+                                    height={500}
+                                    src={picture}
+                                    alt="user icon"
+                                />
+                                : <DefaultUserIcon />
+                        }
+                    </div>
+            }
+        </>
     )
 }
