080576f814cc17f7ae4819514cf88aa8d7c4bdcf
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-05 18:17:50 -0400

    Update: Convert date prop into new Date()

--- client/app/(libs)/getFullDateWithoutWeekDate.ts
+++ client/app/(libs)/getFullDateWithoutWeekDate.ts
@@ -1,3 +1,3 @@
 export default function getFullDateWithoutWeekDate(date: Date) {
-    return date.toLocaleDateString('en-us', { "year": "numeric", "month": "short", "day": "numeric" })
+    return new Date(date).toLocaleDateString('en-us', { "year": "numeric", "month": "short", "day": "numeric" })
 }
