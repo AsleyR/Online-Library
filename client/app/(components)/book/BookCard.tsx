28f75aa330526ff82d63699aa92ff0c2cbcee65f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-08-22 21:55:52 -0400

    Add str length limit to books titles

--- client/app/(components)/book/BookCard.tsx
+++ client/app/(components)/book/BookCard.tsx
@@ -1,8 +1,10 @@
+import limitString from "@/app/(libs)/limitString"
 import { books } from "@prisma/client"
 import Image from "next/image"
 
 export default function BookCard({ book }: { book: books }) {
     const defaultBookCover = '/images/default-book-cover.png'
+    const bookTitle = limitString(book.title)
 
     return (
         <div className="bg-gray-100 border border-gray-300 rounded-lg drop-shadow-md w-full hover:scale-105 transition-all">
@@ -16,7 +18,8 @@ export default function BookCard({ book }: { book: books }) {
                 />
             </div>
             <div className="p-5">
-                <h1 className='font-bold text-lg'>{book.title}</h1>
+                <h1 className='font-bold hidden md:block text-lg'>{bookTitle}</h1>
+                <h1 className='font-bold md:hidden text-lg'>{book.title}</h1>
                 <h3 className='font-text-lg'>{book.author}</h3>
                 <p className='italic font-light'>{book.bookReleaseDate}</p>
             </div>
