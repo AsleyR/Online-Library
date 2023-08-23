5d519cb1dee267dd1b0e267d1f43245571ad21b6
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-08-22 21:43:36 -0400

    Add gray bg to user related pages

--- client/app/user/layout.tsx
+++ client/app/user/layout.tsx
@@ -2,8 +2,8 @@ import Container from "../(components)/Container";
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
     return (
-        <section className="" >
-            <Container>
+        <section className="bg-gray-100" >
+            <Container className="my-0 py-[2rem]">
                 {children}
             </Container>
         </section>
